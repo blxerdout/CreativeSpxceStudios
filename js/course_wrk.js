cdceb1a5f2aef3b0d46996ac0c7430f81dec9c98
blxerdout <blxerdout@gmail.com>
2023-07-04 12:29:31 -0500

    Uploading new Files for the CSXS

---
+++ js/course_wrk.js
@@ -0,0 +1,48 @@
+var counter = 3;
+
+while (counter > 0) {
+    console.log(counter);
+    counter = counter - 1;
+}
+
+console.log("HappY new Years")
+
+/*for(var i = 5; i < 10; i++) {
+    console.log(i)
+}
+
+for(var i = 1; i<= 3; i++) {
+
+    for(var j = 1; j <=5; j++)  {
+        console.log("Week " + i + "day " +j)
+    }
+}*/
+
+for(var wholeNum = 1; wholeNum <= 5; wholeNum++) {
+    
+    for(var multi = 0; multi < 10; multi++) {
+        console.log(wholeNum + "times" + multi + "equals " + wholeNum * multi);
+    } 
+}
+
+let newTime = "12:00";
+
+if (newTime <= "10:00" ) {
+    console.log("Alarm")
+}
+
+else if( newTime && "12:00") {
+    console.log("Your Late")
+}
+
+
+var cubes = 'ABCDEFG';
+//styling console output using CSS with a %c format specifier
+for (var i = 0; i < 7; i++) {
+    var styles = "font-size: 40px; border-radius: 10px; border: 1px solid blue; background: pink; color: purple";
+    console.log("%c" + cubes[i], styles)
+}
+
+for(var i = 5; i < 200; i++) {
+    console.log("I remember.")
+}
---
+++ js/test_file.js
@@ -0,0 +1,33 @@
+console.log("hello world")
+console.log("People Soft")
+ var test = typeof(true)
+ console.log(test)
+
+
+ var car = {};
+ car.milage = 98765;
+ car.color = "red";
+ console.log(car)
+ car.turnTheKey = function() {
+    console.log("The engine is running")
+ }
+ car.theLightsOn = function() {
+    console.log("The lights are on")
+ }
+ console.log(car);
+ car.turnTheKey();
+car.theLightsOn()
+
+var videoGame = {};
+videoGame.genre = "Fighting Games";
+videoGame.platform = "Playstation 5";
+console.log(videoGame)
+videoGame.powerOn = function() {
+   console.log("Playstation 5 powered on")
+}
+videoGame.powerOff = function() {
+   console.log("Playstation 5 powered off")
+}
+console.log(videoGame)
+videoGame.powerOn();
+videoGame.powerOff()
